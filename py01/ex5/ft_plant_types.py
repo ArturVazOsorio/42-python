e1609f74f0bcace2521f7863f882c71a4d5619e8
aantela- <aantela-@student.42porto.com>
2026-08-05 16:42:07 +0100

    asaaaa

--- py01/ex5/ft_plant_types.py
+++ py01/ex5/ft_plant_types.py
@@ -12,22 +12,53 @@
 # *************************************************************************** #
 
 class Plant:
-    def __init__(self, name: str, height: float = 0.0, day: int = 0) -> None:
+    def __init__(self, name: str, height: float = 0.0, days: int = 0) -> None:
         self.name = name
-        self.height = round(float(height), 1)
-        self.day = int(day)
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            self._height = 0.0
+        else:
+            self._height = float(height)
+        if days < 0:
+            print(f"{self.name}: Error, age can't be negative")
+            self._days = 0
+        else:
+            self._days = int(days)
+
+        print(f"Plant created: {self}")
 
     def __str__(self) -> str:
         return (f"{self.name}: {self.height}cm, {self.day} days old")
 
-    def grow(self, size: float = 0.0) -> None:
-        self.height = round(self.height + size, 1)
-
-    def days_up(self, days: int = 0) -> None:
-        self.day = self.day + days
-
     def show(self) -> None:
         print(self)
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._days
+
+    def set_height(self, height: float) -> bool:
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            print("Height update rejected")
+            return False
+        self._height = round(float(height), 1)
+        val = int(self._height) if self._height % 1 == 0 else self._height
+        print(f"Height updated: {val}cm")
+        return True
+
+    def set_age(self, days: int) -> bool:
+        if days < 0:
+            print(f"{self.name}: Error. age can't be negative")
+            print("Age update rejected")
+            return False
+        self._days = days
+        print(f"Age updated : {self._days} days")
+        return True
+
+    def grow (self)
 
 
 if __name__ == "__main__":
